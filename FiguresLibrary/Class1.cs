7b7b475f3d1a30b4ce17d2d30a023a1a97c8430f
ttrueffif <ttrueffif@mail.ru>
2022-07-02 13:09:56 +0400

    Add project files.

---
+++ FiguresLibrary/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace FiguresLibrary
+{
+    public abstract class Figure
+    {
+        public abstract double CalculateArea();
+    }
+
+    // Треугольник
+    public class Triangle : Figure
+    {
+        private double[] _sides = new double[3];
+
+        public Triangle(params double[] sides)
+        {
+            if (sides == null || sides.Length != 3)
+                throw new ArgumentException("Triangle(): invalid parametres");
+
+            for (int i = 0; i < sides.Length; i++)
+                // Так лучше double не сравнивать
+                if (sides[i] >= 0.0) _sides[i] = sides[i];
+                else throw new Exception("??? side length <= 0 ???");
+        }
+
+        // Проверка осуществляется с помощью теоремы Пифагора
+        public bool isRectangular()
+        {
+            // Сторону с максимальной стороной в конец массива _sides
+            for (int i = 0; i < _sides.Length - 1; ++i)
+            {
+                if (_sides[i] > _sides[_sides.Length - 1])
+                {
+                    double temp = _sides[i];
+                    _sides[i] = _sides[_sides.Length - 1];
+                    _sides[_sides.Length - 1] = temp;
+                }
+            }
+
+            // true - треугольник прямоугольный
+            // false - треугольник не прямоугольный
+            return _sides[2] * _sides[2] == (_sides[0] * _sides[0] + _sides[1] * _sides[1]);
+        }
+
+        // Нахождение периметра, вспомогательный метод
+        // закрыт, использую только в методе CalculateArea()
+        private double CalculateP()
+        {
+            double P = default(double);
+
+            for (int i = 0; i < _sides.Length; ++i)
+                P += _sides[i];
+
+            return P;
+        }
+
+        public override double CalculateArea()
+        {
+            // Полупериметр для формулы Герона
+            // Формула Герона 
+            double P = CalculateP() / 2;
+
+            return Math.Sqrt(P * (P - _sides[0]) * (P - _sides[1]) * (P - _sides[2]));
+        }
+    }
+
+    // Круг
+    public class Circle : Figure
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
---
+++ FiguresLibraryTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FiguresLibraryTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FiguresLibraryTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f1d6e7b3-f181-4568-8f71-2a13da85edcd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
